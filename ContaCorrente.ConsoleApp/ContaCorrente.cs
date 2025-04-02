ff7c73116f550d11bbaaf587c4087122e6128bdb
AlexAraldi <alexanderaraldi@gmail.com>
2025-04-02 17:25:32 -0300

    Add project files.

---
+++ ContaCorrente.ConsoleApp/ContaCorrente.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace ContaCorrente.ConsoleApp
+{
+    class ContaCorrente
+    {
+        public int numeroConta;
+
+        public decimal saldo;
+
+        public void Sacar(decimal valor)
+        {
+            saldo -= valor;
+        }
+        
+        public void Depositar(decimal valor)
+        {
+            saldo += valor;
+        }
+        
+    }
+}
---
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ContaCorrente.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta1 = new ContaCorrente();
+            conta1.numeroConta = 1;
+            conta1.saldo = 1000;
+            conta1.Sacar(200);
+            conta1.Depositar(200);
+        }
+    }
+}
